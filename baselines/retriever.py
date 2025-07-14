0aac59e3a6aa314a5e3ddce13a7e2f1a2a3c9b32
Double680 <maoqy0503@mail.ustc.edu.cn>
2025-07-14 22:04:03 +0800

    update table ndcg bugs

--- baselines/retriever.py
+++ baselines/retriever.py
@@ -38,9 +38,10 @@ class GroundTruthRetriever:
 
 
 class DensePassageRetriever:
-    def __init__(self, path_root, aug='none', top_k=10, gpu=0):
+    def __init__(self, path_root, aug='none', tabheader=False, top_k=10, gpu=0):
         self.path_root = path_root
         self.aug = aug
+        self.tabheader = tabheader
         self.top_k = top_k
         self.device = f"cuda:{gpu}" if gpu != -1 else "cpu"
         self.sim_func = nn.CosineSimilarity(dim=-1)
@@ -51,7 +52,14 @@ class DensePassageRetriever:
         with open(doc_emb_path, "r") as file:
             emb_dict = json.loads(file.read())
         text_st_embs = torch.tensor(emb_dict["text_embs"]).to(self.device)
-        table_st_embs = torch.tensor(emb_dict["table_embs"]).to(self.device)
+        if self.tabheader:
+            table_header_emb_path = os.path.join(self.path_root, uid, "table_header_embs.json")
+            with open(table_header_emb_path, "r") as file:
+                header_emb_dict = json.loads(file.read())
+            table_col_embs = torch.tensor(header_emb_dict["table_col_embs"]).to(self.device)
+            table_row_embs = torch.tensor(header_emb_dict["table_row_embs"]).to(self.device)
+        else:
+            table_st_embs = torch.tensor(emb_dict["table_embs"]).to(self.device)
         if self.aug != 'none':
             query_emb_path = os.path.join(self.path_root, uid, f"{self.aug}_query_embs.json")
         else:
@@ -74,23 +82,23 @@ class DensePassageRetriever:
         update_text_inds = sorted(list(set(text_table_inds).union(set(retrieved_text_inds))))
         update_texts = [sample["paragraphs"][ind] for ind in update_text_inds]
         
-        tables, table_desc = sample['tables'], sample['table_description']
-        table_desc_st = [(int(key.split('-')[0]), table_desc[key]) for key in table_desc]
-
-        table_scores = self.sim_func(question_emb, table_st_embs)
-        try:
-            retrieved_table_inds = sorted(torch.topk(table_scores, k=self.top_k).indices.tolist())
-        except Exception:
-            retrieved_table_inds = [i for i in range(len(table_scores))]
-        update_table_desc = [table_desc_st[ind] for ind in retrieved_table_inds]
-        update_table_dict = {i: [] for i in range(len(tables))}
-        for table_id, desc in update_table_desc:
-            update_table_dict[table_id].append(desc.split(f"Table {table_id} shows ")[-1])
-        update_tables = ["\n".join(update_table_dict[i]) for i in range(len(tables))]    
+        if self.tabheader:
+            col_sim_scores = self.sim_func(question_emb, table_col_embs)
+            col_indices = torch.topk(col_sim_scores, k=min(self.top_k, col_sim_scores.size(1))).indices.squeeze(0)
+            row_sim_scores = self.sim_func(question_emb, table_row_embs)
+            row_indices = torch.topk(row_sim_scores, k=min(self.top_k, row_sim_scores.size(1))).indices.squeeze(0)
+        else:
+            tables, table_desc = sample['tables'], sample['table_description']
+            table_desc_st = [(int(key.split('-')[0]), table_desc[key]) for key in table_desc]
+            table_scores = self.sim_func(question_emb, table_st_embs)
+            retrieved_table_inds = torch.topk(table_scores, k=min(self.top_k, len(table_scores))).indices.tolist()
+            update_table_desc = [table_desc_st[ind] for ind in retrieved_table_inds]
+            update_table_dict = {i: [] for i in range(len(tables))}
+            for table_id, desc in update_table_desc:
+                update_table_dict[table_id].append(desc.split(f"Table {table_id} shows ")[-1])
+            update_tables = ["\n".join(update_table_dict[i]) for i in range(len(tables))]    
 
         return update_texts, update_tables, retrieved_text_inds, retrieved_table_inds
-
-        # table_scores = self.sim_func(question_emb, table_st_embs)
-        # table_inds = torch.where(table_scores > self.top_p)[0]
+        
          
         
