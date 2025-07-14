8a8845de8e426a07fea644f0f9e84869acf27aba
Double680 <maoqy0503@mail.ustc.edu.cn>
2025-07-14 21:29:52 +0800

    update emb store script

--- augment/store_emb.py
+++ augment/store_emb.py
@@ -1,8 +1,13 @@
+import sys
+sys.path.append('./')
+
 import argparse
 import torch
 import json
 import os
 from augment.retriever import Retriever
+from augment.table_tree_docs import *
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dev', action='store_true')
@@ -21,7 +26,7 @@ with open(f"datasets/multihiertt/{dataset_type}.json", "r") as file:
     data = json.loads(file.read())
 
 if args.doc:
-    for item in data:
+    for item in tqdm(data):
         uid = item['uid']
         save_root = f'stored/{uid}'
         if not os.path.exists(save_root):
@@ -36,8 +41,32 @@ if args.doc:
         with open(f"{save_root}/doc_embs.json", "w") as file:
             file.write(json.dumps(doc_sample))
 
+if args.tabheader:
+    with open(f"datasets/multihiertt/{dataset_type}_headers.json", "r") as file:
+        header_dict = json.loads(file.read())
+    for item in tqdm(data):
+        uid = item['uid']
+        save_root = f'stored/{uid}'
+        if not os.path.exists(save_root):
+            os.mkdir(save_root)
+        table_header = header_dict[uid]
+        table_docs = get_table_tree_docs(table_header)
+        col_doc_texts, row_doc_texts = [], []
+        for table in table_docs:
+            col_doc_texts += [table['col_docs'][id]['text'] for id in range(len(table['col_docs']))]
+            row_doc_texts += [table['row_docs'][id]['text'] for id in range(len(table['row_docs']))]
+        col_embs = ret.get_emb(col_doc_texts).tolist()
+        row_embs = ret.get_emb(row_doc_texts).tolist()
+        header_embs = {
+            "table_col_embs": col_embs,
+            "table_row_embs": row_embs
+        }
+        with open(f"{save_root}/table_header_embs.json", "w") as file:
+            file.write(json.dumps(header_embs))
+
+
 if args.query:
-    for item in data:
+    for item in tqdm(data):
         uid = item['uid']
         save_root = f'stored/{uid}'
         if not os.path.exists(save_root):
@@ -53,9 +82,11 @@ if args.raw_aug:
     with open(f"datasets/multihiertt/{dataset_type}_raw_aug.jsonl", "r") as file:
         query_data = [json.loads(item) for item in file.readlines()]
 
-    for item in query_data:
+    for item in tqdm(query_data):
         uid = item['uid']
         save_root = f'store/{uid}'
+        if not os.path.exists(save_root):
+            os.mkdir(save_root)
         query_embs = ret.get_emb(item['new_query'], query_type=True).tolist()
         sample = {
             "query_embs": query_embs
@@ -67,9 +98,11 @@ if args.grpo_aug:
     with open(f"datasets/multihiertt/{dataset_type}_grpo_aug.jsonl", "r") as file:
         query_data = [json.loads(item) for item in file.readlines()]
 
-    for item in query_data:
+    for item in tqdm(query_data):
         uid = item['uid']
         save_root = f'store/{uid}'
+        if not os.path.exists(save_root):
+            os.mkdir(save_root)
         query_embs = ret.get_emb(item['new_query'], query_type=True).tolist()
         sample = {
             "query_embs": query_embs
